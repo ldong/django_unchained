7b4d556594433cb58f4b975c15d563d1651e861d
Lin Dong <lindong@gatech.edu>
2014-03-04 22:09:23 -0500

    Continue to watch video until video 6

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 1. Prerequisites:  Django sqlite3
+
 2. Django uses relationship database, and combile necessary query commands.
 
+3. Meta class for customizing plural
--- stories/admin.py
+++ stories/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from stories.models import Story
 
 # Register your models here.
+
+class StoryAdmin(admin.ModelAdmin):
+    list_display=('__unicode__', 'url', 'moderator', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('title', 'moderator__username', 'moderator__first_name'
+            ,'moderator__last_name')
+    # def lower_case_title(self, obj):
+        # return obj.title.lower()
+    # lower_case_title.short_description = 'title'
+
+admin.site.register(Story, StoryAdmin)
